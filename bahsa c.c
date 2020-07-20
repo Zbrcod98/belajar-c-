e6ec1a8ea7c26995639a587507a89c232451e7f7
Zbrcod98 <52246118+Zbrcod98@users.noreply.github.com>
2020-07-20 14:37:42 +0700

    Add files via upload

---
+++ bahsa c.c
@@ -0,0 +1,12 @@
+
+for(int i= 0;i<10; i=i+1)
+{a[i] =0;}
+ for(i=0; i,10;i++)
+ {b[i] = a[i]=1};
+for (i=0;i<10;i++)
+{c[i] = b[i]+i;}
+ for (int i =o; i<10;i++)
+	 n[i]=0
+ for (i+0;1<10;i++)
+	 Print "i" "n[i]"
+ 
---
+++ test.py
@@ -0,0 +1,4 @@
+a=34
+y=54
+print(a*y)
+print ("hai nama saya ")
